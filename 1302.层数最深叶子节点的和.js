7c8604e04c48005b73e94bf09a86a64be1e5a8e3
Leo <leoaee@gmail.com>
2021-06-04 22:13:41 +0800

    init commit

---
+++ 1302.层数最深叶子节点的和.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1302 lang=javascript
+ *
+ * [1302] 层数最深叶子节点的和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var deepestLeavesSum = function(root) {
+
+};
+// @lc code=end
+
---
+++ 808.分汤.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=808 lang=javascript
+ *
+ * [808] 分汤
+ */
+
+// @lc code=start
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var soupServings = function(N) {
+
+};
+// @lc code=end
+
